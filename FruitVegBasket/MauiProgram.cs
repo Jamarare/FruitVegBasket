ee85d2e364e36006843811752205c4df0a12b773
opilane <opilane@HP-CZC0359NN9>
2025-03-10 13:43:24 +0200

    part - 5

---
+++ FruitVegBasket/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Maui;
+using FruitVegBasket.Pages;
+using FruitVegBasket.Services;
+using FruitVegBasket.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace FruitVegBasket;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Ubuntu-Regular.ttf", "UbuntuRegular");
+				fonts.AddFont("Ubuntu-Bold.ttf", "UbuntuBold");
+			})
+            .UseMauiCommunityToolkit();
+
+		builder.Services.AddSingleton<CategoryService>();
+		builder.Services.AddSingleton<HomePageViewModel>();
+		builder.Services.AddSingleton<HomePage>();
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
