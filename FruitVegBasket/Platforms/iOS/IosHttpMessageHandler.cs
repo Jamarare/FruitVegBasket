02183bc7204e83d6af49192c7cfface0b6f2ce3f
opilane <opilane@HP-CZC0359NN9>
2025-03-14 13:41:45 +0200

    part -6

---
+++ FruitVegBasket/Platforms/iOS/IosHttpMessageHandler.cs
@@ -0,0 +1,15 @@
+﻿using FruitVegBasket.Interfaces;
+using Security;
+using System;
+
+namespace FruitVegBasket.Platforms.iOS
+{
+    class IosHttpMessageHandler : IPlatformHttpMessageHandler
+    {
+        public HttpMessageHandler GetHttpMessageHandler() =>
+             new NSUrlSessionHandler
+             {
+                 TrustOverrideForUrl = (NSUrlSessionHandler sender, string url, SecTrust trust) => url.StartsWith("https://localhost")
+             };
+    }
+}
---
+++ FruitVegBasket/Services/OffersService.cs
@@ -0,0 +1,39 @@
+﻿using FruitVegBasket.Constants;
+using FruitVegBasket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FruitVegBasket.Services
+{
+    public class OffersService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OffersService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IEnumerable<Offer>> GetActiveOffersAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient(AppConstants.HttpClientName);
+
+            var response = await httpClient.GetAsync("/masters/offers");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                if (!string.IsNullOrEmpty(content))
+                {
+                    return JsonSerializer.Deserialize<IEnumerable<Offer>>(content, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+                }
+            }
+            return Enumerable.Empty<Offer>();
+        }
+    }
+}
